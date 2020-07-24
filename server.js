bd6bf4f21a85a2261cc38cc07b8d59fb107f7379
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-07-23 22:49:11 -0700

    prepare for deployment to heroku!

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use("/api/v1/users", require("./api/v1/users")); // use users.js route
 app.use("/api/v1/all-user-tech", require("./api/v1/all-user-tech")); // use allUsers.js route
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'); // ./client/build/index.html
+});
 
 const port = process.env.PORT || 1337;
 
