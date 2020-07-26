c54c9444d535f18a1476c717f65047180784baf0
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-07-25 18:36:35 -0700

    refine api calls

--- client/src/components/pages/Connect.js
+++ client/src/components/pages/Connect.js
@@ -131,73 +131,74 @@ class Connect extends React.Component {
       //     console.log(error);
       //   });
 
-      axios
-        .get("/api/v1/all-user-tech")
-        .then((res) => {
-          const allUserTechnologies = res.data;
-          console.log("this is all user tech: ", allUserTechnologies);
-          axios
-            .get("/api/v1/users")
-            .then((res2) => {
-              const users = res2.data.map((user) => {
-                return {
-                  id: user.id,
-                  handle: user.handle,
-                  email: user.email,
-                  createdAt: user.created_at,
-                  technologies: allUserTechnologies
-                    .filter((technology) => technology.userId === user.id)
-                    .map((tech) => tech.technologyName),
-                };
-              });
+      // axios
+      //   .get("/api/v1/all-user-tech")
+      //   .then((res) => {
+      //     const allUserTechnologies = res.data;
+      //     console.log("this is all user tech: ", allUserTechnologies);
+      //     axios
+      //       .get("/api/v1/users")
+      //       .then((res2) => {
+      //         const users = res2.data.map((user) => {
+      //           return {
+      //             id: user.id,
+      //             handle: user.handle,
+      //             email: user.email,
+      //             createdAt: user.created_at,
+      //             technologies: allUserTechnologies
+      //               .filter((technology) => technology.userId === user.id)
+      //               .map((tech) => tech.technologyName),
+      //           };
+      //         });
 
-              console.log("this is local users were working with: ", users);
-              const currentUserTechNames = this.props.currentUser.techInterestedIn.map(
-                (tech) => tech.name
-              );
-              console.log("currentUserTechNames: ", currentUserTechNames);
-              const filteredUsers = [];
-              currentUserTechNames.forEach((currentUserTechName) => {
-                users.forEach((user) => {
-                  user.technologies.forEach((techName) => {
-                    // console.log(user);
-                    if (techName === currentUserTechName) {
-                      // return multiple copies of users with the same tech name
-                      filteredUsers.push(user);
-                    }
-                  });
-                });
-              });
-              // console.log(users);
-              // const bestMatchedUsers = filteredUsers;
-              // console.log("testing: ", filteredUsers);
-              // const bestMatchedUsers = [];
-
-              //  - set displayed users state to filtered users
-              /* TODO order filteredUsers by most common tech interests, count how often someone is matched, order by number of times matches, most = highest, less = lowest. map through filteredUsers, get each user. if user shows up multiple times, push to top of list */
-
-              const bestMatched = countBy(filteredUsers, "handle");
-              const orderedUsers = orderBy(filteredUsers, ["handle"], ["desc"]);
-              const bestMatchedUsers = filteredUsers;
-              console.log(bestMatchedUsers);
-
-              console.log("this is filtered users: ", bestMatched);
-
-              this.setState({
-                displayedUsers: [...new Set(filteredUsers)],
-              });
-            })
-            .catch((error) => {
-              // handle error
-              console.log(error);
-            });
-        })
-        .catch((error) => {
-          // handle error
-          console.log(error);
+      // console.log("this is local users were working with: ", users);
+      const currentUserTechNames = this.props.currentUser.techInterestedIn.map(
+        (tech) => tech.name
+      );
+      console.log("currentUserTechNames: ", currentUserTechNames);
+      const filteredUsers = [];
+      currentUserTechNames.forEach((currentUserTechName) => {
+        this.state.displayedUsers.forEach((user) => {
+          user.technologies.forEach((techName) => {
+            // console.log(user);
+            if (techName === currentUserTechName) {
+              // return multiple copies of users with the same tech name
+              filteredUsers.push(user);
+            }
+          });
         });
+      });
+      // console.log(users);
+      // const bestMatchedUsers = filteredUsers;
+      // console.log("testing: ", filteredUsers);
+      // const bestMatchedUsers = [];
+
+      //  - set displayed users state to filtered users
+      /* TODO order filteredUsers by most common tech interests, count how often someone is matched, order by number of times matches, most = highest, less = lowest. map through filteredUsers, get each user. if user shows up multiple times, push to top of list */
+
+      const bestMatched = countBy(filteredUsers, "handle");
+      const orderedUsers = orderBy(filteredUsers, ["handle"], ["desc"]);
+      const bestMatchedUsers = filteredUsers;
+      console.log(bestMatchedUsers);
+
+      console.log("this is filtered users: ", bestMatched);
+
+      this.setState({
+        displayedUsers: [...new Set(filteredUsers)],
+      });
     }
   }
+  // .catch((error) => {
+  //   // handle error
+  //   console.log(error);
+  // });
+  // })
+  //     .catch((error) => {
+  //       // handle error
+  //       console.log(error);
+  //     });
+  // }
+  // }
 
   handleChange(e) {
     this.setState({ search: e.target.value });
