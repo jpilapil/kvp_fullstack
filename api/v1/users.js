b5fa124fc9719cf15008f6b38ad1e3f36d0b8230
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-08-09 18:21:36 -0700

    sign up validation

--- api/v1/users.js
+++ api/v1/users.js
@@ -2,90 +2,111 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db");
-const allUsers = require("../../queries/allUsers");
-const { toJson, toSafeParse, toHash } = require("../../utils/helpers");
+// const allUsers = require("../../queries/allUsers");
+const { toHash } = require("../../utils/helpers");
 const insertUser = require("../../queries/insertUser");
 const insertXrefUserTech = require("../../queries/insertXrefUserTech");
 const { v4: getUuid } = require("uuid");
+const getSignUpEmailError = require("../../validation/getSignUpEmailError");
+const getSignUpPasswordError = require("../../validation/getSignUpPasswordError");
 
 // @route      GET api/v1/users
 // @desc       GET a valid user via email and password
 // @access     PUBLIC
 
-router.get("/", (req, res) => {
-  // console.log(req.query);
-  db.query(allUsers)
-    .then((dbRes) => {
-      const users = toSafeParse(toJson(dbRes)); // get response and turn it into json, then parse json to convert to array
-      console.log(users);
-      res.json(users);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-});
+// router.get("/", (req, res) => {
+//   // console.log(req.query);
+//   db.query(allUsers)
+//     .then((dbRes) => {
+//       const users = toSafeParse(toJson(dbRes)); // get response and turn it into json, then parse json to convert to array
+//       console.log(users);
+//       res.json(users);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(400).json(err);
+//     });
+// });
 
 // @route      POST api/v1/users
 // @desc       Create a new user and user xref tech
 // @access     PUBLIC
 router.post("/", async (req, res) => {
-  const user = {
-    id: req.body.id,
-    handle: req.body.handle,
-    email: req.body.email,
-    password: await toHash(req.body.password),
-    gender: req.body.gender,
-    created_at: req.body.createdAt,
-  };
+  // use destructure to avoid repeat of req.body
+  const {
+    id,
+    handle,
+    email,
+    password,
+    gender,
+    createdAt,
+    techInterestedIn,
+  } = req.body;
+  const emailError = getSignUpEmailError(email);
+  const passwordError = getSignUpPasswordError(password);
 
-  const userXrefTech1 = {
-    id: getUuid(),
-    user_id: req.body.id,
-    technology_id: req.body.techInterestedIn[0].id,
-  };
+  if (emailError === "" && passwordError === "") {
+    const user = {
+      id: id,
+      handle: handle,
+      email: email,
+      password: await toHash(password),
+      gender: gender,
+      created_at: createdAt,
+    };
 
-  const userXrefTech2 = {
-    id: getUuid(),
-    user_id: req.body.id,
-    technology_id: req.body.techInterestedIn[1].id,
-  };
+    const userXrefTech1 = {
+      id: getUuid(),
+      user_id: id,
+      technology_id: techInterestedIn[0].id,
+    };
 
-  const userXrefTech3 = {
-    id: getUuid(),
-    user_id: req.body.id,
-    technology_id: req.body.techInterestedIn[2].id,
-  };
-  console.log(user);
+    const userXrefTech2 = {
+      id: getUuid(),
+      user_id: id,
+      technology_id: techInterestedIn[1].id,
+    };
 
-  db.query(insertUser, user)
-    .then((dbRes) => {
-      db.query(insertXrefUserTech, userXrefTech1)
-        .then((res) => {
-          db.query(insertXrefUserTech, userXrefTech2)
-            .then((res) => {
-              db.query(insertXrefUserTech, userXrefTech3)
-                .then((res) => {
-                  console.log(res);
-                })
-                .catch((err) => {
-                  console.log(err);
-                });
-              console.log(res);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-          console.log(res);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      console.log(dbRes);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    const userXrefTech3 = {
+      id: getUuid(),
+      user_id: id,
+      technology_id: techInterestedIn[2].id,
+    };
+
+    console.log(user);
+
+    db.query(insertUser, user)
+      .then((dbRes) => {
+        db.query(insertXrefUserTech, userXrefTech1)
+          .then((res) => {
+            db.query(insertXrefUserTech, userXrefTech2)
+              .then((res) => {
+                db.query(insertXrefUserTech, userXrefTech3)
+                  .then((res) => {
+                    console.log(res);
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+                console.log(res);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        console.log(dbRes);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json({ emailError, passwordError });
+      });
+  } else {
+    res.status(400).json({ emailError, passwordError });
+  }
 });
 
 module.exports = router;
