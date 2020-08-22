e3a1e899d79801f33717586d6cfdc271f0ee425c
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-08-21 17:24:25 -0700

    fix heroku bug!!!!!!!!!!!!!!!!!!!!!!!!!

--- api/v1/users.js
+++ api/v1/users.js
@@ -30,6 +30,7 @@ router.get("/", (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
+      ``;
     });
 });
 
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use("/api/v1/all-user-tech", require("./api/v1/all-user-tech")); // use allU
 app.use("/api/v1/technologies", require("./api/v1/technologies"));
 app.use(express.static("client/build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname), "client", "build", "index.html"); // ./client/build/index.html
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); // ./client/build/index.html
 });
 
 const port = process.env.PORT || 4444;
