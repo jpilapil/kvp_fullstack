4cb767779558ef0ef3ae2b41d985c5e591e98742
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-08-25 16:57:40 -0700

    debugging tech id and names

--- client/src/components/pages/Connect.js
+++ client/src/components/pages/Connect.js
@@ -25,23 +25,23 @@ class Connect extends React.Component {
   componentDidMount() {
     // componentDidMount is a lifecycle method, does not need to be called somewhere else, will always run before render
 
-    // // get signed in user from redux store
-    // axios
-    //   .get(
-    //     "https://raw.githubusercontent.com/jpilapil/key-value-pair/master/src/mock-data/user.json"
-    //   )
-    //   .then((res) => {
-    //     const currentUser = res.data;
-    //     console.log("this is the user stored to global state: ", currentUser);
-    //     this.props.dispatch({
-    //       type: actions.UPDATE_CURRENT_USER,
-    //       payload: res.data,
-    //     });
-    //   })
-    //   .catch((error) => {
-    //     // handle error
-    //     console.log(error);
-    //   });
+    // get signed in user from redux store
+    axios
+      .get(
+        "https://raw.githubusercontent.com/jpilapil/key-value-pair/master/src/mock-data/user.json"
+      )
+      .then((res) => {
+        const currentUser = res.data;
+        console.log("this is the user stored to global state: ", currentUser);
+        this.props.dispatch({
+          type: actions.UPDATE_CURRENT_USER,
+          payload: res.data,
+        });
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
 
     // get all users with technology names and set them do displayedUsers & allUsers state
     axios
@@ -58,7 +58,10 @@ class Connect extends React.Component {
               createdAt: user.created_at,
               technologies: userTechnologies
                 .filter((technology) => technology.userId === user.id)
-                .map((tech) => tech.technologyName),
+                .map((tech) => tech.technologyId),
+              // technologies: userTechnologies
+              //   .filter((technology) => technology.userId === user.id)
+              //   .map((tech) => tech.technologyName),
             };
           });
           this.setState({
@@ -80,9 +83,12 @@ class Connect extends React.Component {
       });
     } else {
       // if filter = all matched users, set state of displayedUsers to filtered users
-      const currentUserTechNames = this.props.currentUser.techInterestedIn.map(
-        (tech) => tech.name
-      );
+      const currentUserTechNames = this.props.currentUser.techInterestedIn
+        .map((tech) => tech)
+        .map((id) => id.id);
+      // const currentUserTechNames = this.props.currentUser.techInterestedIn.map(
+      //   (tech) => tech.name
+      // );
       console.log("currentUserTechNames: ", currentUserTechNames);
       const filteredUsers = [];
       currentUserTechNames.forEach((currentUserTechName) => {
